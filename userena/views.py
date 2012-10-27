2b2ccafaaff1606444f9aa44e557a45ffb1770c4
Oleg Dolya <oleg.dolya@gmail.com>
2012-10-27 15:43:39 +0300

    fix bug create profile

--- userena/views.py
+++ userena/views.py
@@ -617,8 +617,9 @@ def profile_detail(request, username, template_name=userena_settings.USERENA_PRO
     try:
         profile = user.get_profile()
     except profile_model.DoesNotExist:
-        profile = profile_model.create(user=user)
-
+        #profile = profile_model.create(user=user)
+        profile = profile_model(user=user)
+        profile.save()
     if not profile.can_view_profile(request.user):
         return HttpResponseForbidden(_("You don't have permission to view this profile."))
     if not extra_context: extra_context = dict()
